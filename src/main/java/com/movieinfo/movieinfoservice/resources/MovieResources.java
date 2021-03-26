ca6cd33d3263a392a9ee5648185a126d6036470a
mohammed.totah <mohammed.totah@jesoft.io>
2021-03-26 12:53:22 +0200

    moovie info service

---
+++ src/main/java/com/movieinfo/movieinfoservice/resources/MovieResources.java
@@ -0,0 +1,28 @@
+package com.movieinfo.movieinfoservice.resources;
+
+import com.movieinfo.movieinfoservice.models.Movie;
+import com.movieinfo.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResources {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${api.key}")
+    private String apiKey;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable String movieId){
+        MovieSummary movieSummary =
+                restTemplate.getForObject
+                        ("https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+apiKey, MovieSummary.class);
+        return new Movie(movieId,movieSummary.getTitle() , movieSummary.getOverview());
+    }
+}
